8f9473d2a1f8591b5863f1041a5be4d69a396162
Draksha Chaudhary <draksha_ug_23@cse.nits.ac.in>
2026-09-08 04:04:24 +0530

    Fix CORS preflight for production frontend

--- api/main.py
+++ api/main.py
@@ -20,11 +20,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-        "https://churn-frontend-production.up.railway.app"
-    ],
+    # allow_origins=[
+    #     "http://127.0.0.1:5500",
+    #     "http://localhost:5500"
+    #     "https://churn-frontend-production.up.railway.app"
+    # ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
