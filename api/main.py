a0920b38303bf9849b4bdbf0805d74c1492ac89d
Draksha Chaudhary <draksha_ug_23@cse.nits.ac.in>
2026-09-08 04:12:03 +0530

    Fix production CORS

--- api/main.py
+++ api/main.py
@@ -18,18 +18,19 @@ app = FastAPI(
 
 # CORS SETTINGS
 
+# CORS SETTINGS
+
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=[
-    #     "http://127.0.0.1:5500",
-    #     "http://localhost:5500"
-    #     "https://churn-frontend-production.up.railway.app"
-    # ],
+    allow_origins=[
+        "https://churn-frontend-production.up.railway.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # FIND PROJECT FOLDERS
 
