236398f9f47b51076bb84257e5a0d939379037d2
Draksha Chaudhary <draksha_ug_23@cse.nits.ac.in>
2026-09-08 03:25:55 +0530

    Configure Railway MySQL connection

--- api/database.py
+++ api/database.py
@@ -7,14 +7,13 @@ load_dotenv()
 
 
 def get_connection():
-
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
     )
-
     return connection
 
 def save_prediction(
