0a5fd40ee637f70ab37c85db2efbc6b6f35000bf
Draksha Chaudhary <draksha_ug_23@cse.nits.ac.in>
2026-09-08 03:47:06 +0530

    Allow production frontend CORS

--- api/main.py
+++ api/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500"
+        "https://churn-frontend-production.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
